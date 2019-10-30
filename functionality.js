3f2e4fe83ccfdc93ff8edc5167c4fd305c0143fa
wintsier <sierrawinter@earthlink.net>
2019-10-30 14:49:54 -0500

    Updates

--- functionality.js
+++ functionality.js
@@ -148,3 +148,17 @@
 			console.log("sending " + json);
 			ws.send(json);
 		}
+
+		function displayChamber(evt, cityName) {
+			var i, tabcontent, tablinks;
+			tabcontent = document.getElementsByClassName("tabcontent");
+			for (i = 0; i < tabcontent.length; i++) {
+				tabcontent[i].style.display = "none";
+			}
+			tablinks = document.getElementsByClassName("tablinks");
+			for (i = 0; i < tablinks.length; i++) {
+				tablinks[i].className = tablinks[i].className.replace(" active", "");
+			}
+			document.getElementById(cityName).style.display = "block";
+			evt.currentTarget.className += " active";
+		}
